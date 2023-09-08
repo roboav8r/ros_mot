217df532a1e28bb578182549a2960e7add90049c
John Duncan <john.a.duncan@utexas.edu>
2023-09-07 19:52:02 -0500

    Detector publisher placeholder added

--- scripts/detector.py
+++ scripts/detector.py
@@ -16,6 +16,7 @@ import torch
 import time
 import rospkg
 
+from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2
 
@@ -24,10 +25,11 @@ from mmdet3d.structures.points import BasePoints
 
 # Constants / parameters
 callback_map = {'PointCloud2': 'self.pc2_callback'}
+publisher_map = {'PointCloud2': 'self.bbarray_pub'}
 
-# A Baseline of 3D Multi-Object Tracking
+# Base detector class
 class mmdetector3d():			  	
-	def __init__(self, name, model, topic, msg_type):                      
+	def __init__(self, name, model, topic, msg_type, pub):                      
 		print('Starting detector: ', name)
 		self.name = name
 		self.model = model
@@ -37,17 +39,37 @@ class mmdetector3d():
 		# Create subscriber
 		self.sub = rospy.Subscriber(self.topic, eval(self.msg_type), eval(callback_map[self.msg_type]))
 
+		# Create publisher
+		self.pub = pub
+
+		# Create empty messages
+		self.bb_array_msg = BoundingBoxArray()
+		self.bb_msg = BoundingBox()
+
 	def pc2_callback(self, pc2_msg):
 		# Format ros pc2 message -> mmdet3d BasePoints
 		pc_list = point_cloud2.read_points_list(pc2_msg)
 		pc_np = np.array(list(pc_list))
 		result, _  = inference_detector(self.model, pc_np)
 
+		# Clear messages
+		self.bb_array_msg = BoundingBoxArray()
+		self.bb_msg = BoundingBox()
+
+		# Populate messages
+		# TODO 
+
+		# Publish messages
+		self.pub.publish(self.bb_array_msg)
+
 if __name__ == '__main__':
 
 	# Initialize node
-	rospy.init_node("lidar_detector")
+	rospy.init_node("detector")
 	print("Detector node initialized")
+
+	# Create common publishers
+	detection3d_pub = rospy.Publisher("detections_3d", BoundingBoxArray, queue_size=10)
 
 	# Get package path
 	pkg_path = rospkg.RosPack().get_path('ros_mot')
@@ -68,6 +90,6 @@ if __name__ == '__main__':
 			model = init_model(cfg_file, ckpt_file, device)
 
 			# Create detector object
-			mmdetector3d(name,model,value['topic'],value['msg_type'])
+			mmdetector3d(name,model,value['topic'],value['msg_type'],detection3d_pub)
 
 	rospy.spin()
