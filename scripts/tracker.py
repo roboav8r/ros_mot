64c3b558b076fff7a5a32a02a6d41440dbe0c841
John Duncan <john.a.duncan@utexas.edu>
2023-09-20 17:55:59 -0500

    Add KF objects, predict steps

--- scripts/tracker.py
+++ scripts/tracker.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 from __future__ import print_function
-import numpy as np, copy, math, sys, argparse
 
 import os
 import sys
-import rospy
-import torch
 import time
-import rospkg
 import math
 
+import torch
 import gtsam
+import numpy as np
+from scipy.optimize import linear_sum_assignment
 
+import rospy
+import rospkg
 import tf2_ros
-
 import geometry_msgs
+
 from tf2_geometry_msgs import PoseStamped
 from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
 
@@ -23,7 +24,8 @@ from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
 class Detection():
     def __init__(self, ts, px, py, pz, prob, label):
         self.timestamp = ts
-        self.px, self.py, self.pz = px, py, pz
+        # self.px, self.py, self.pz = px, py, pz
+        self.pos = np.array([[px], [py], [pz]])
         self.prob = prob
         self.label = label
         # Can be empty/null/contain partial info
@@ -34,14 +36,44 @@ class Detection():
 class Track():
     def __init__(self, det):
         self.timestamp = det.timestamp
-        self.px = det.px
-        self.py = det.py
-        self.pz = det.pz
-        self.vx = 0
-        self.vy = 0
-        self.vz = 0
+        self.pos = det.pos
+        self.vel = np.array([[0.],[0.],[0.]])
         self.prob = det.prob
         self.label = det.label
+        self.vel_variance = 0.5
+
+        # Kalman filter for this object
+        self.kf = gtsam.KalmanFilter(6)
+        self.state = self.kf.init(
+            np.vstack((self.pos, self.vel)),
+            np.diag([self.pos_variance,self.pos_variance,self.pos_variance,self.vel_variance,self.vel_variance,self.vel_variance])
+            # TODO - set this to detection covariance
+        )
+
+        # Process model matrices
+        self.proc_model = np.diag(np.ones(6))
+        self.proc_noise = gtsam.noiseModel.Diagonal.Sigmas([1,1,1,1,1,1])
+    
+    def compute_proc_model(self,dt):
+        self.proc_model[0,3] = dt
+        self.proc_model[1,4] = dt
+        self.proc_model[2,5] = dt
+
+    def compute_proc_noise(self,dt):
+        # TODO - verify noise model coefficients
+        self.proc_noise = gtsam.noiseModel.Diagonal.Sigmas([0.25*self.vel_variance**4*dt**4,
+                                                            0.25*self.vel_variance**4*dt**4,
+                                                            0.25*self.vel_variance**4*dt**4,
+                                                            0.5*self.vel_variance**2*dt**2,
+                                                            0.5*self.vel_variance**2*dt**2,
+                                                            0.5*self.vel_variance**2*dt**2])
+    def predict(self, t):
+        dt = (t - self.timestamp).to_sec()
+        self.timestamp = t
+        
+        self.compute_proc_model(dt)
+        self.compute_proc_noise(dt)
+        self.state = self.kf.predict(self.state,self.proc_model,np.zeros((6,6)),np.zeros((6,1)),self.proc_noise)
 
 # Graph Tracker object
 class GraphTracker():
@@ -56,9 +88,8 @@ class GraphTracker():
         self.tf_listener = tf2_ros.TransformListener(self.tf_buf)
         self.pose_stamped = PoseStamped()
 
-        # Track and detection parameters
+        # Track and detection parameters & variables
         self.detections = []
-        self.trk_candidates = []
         self.tracks = []
         self.trk_delete_thresh = 0.25
         self.trk_delete_age = 10
@@ -66,6 +97,9 @@ class GraphTracker():
         self.trk_create_thresh = 0.75
         self.trk_create_age = 5
         self.trk_pub = pub
+
+        # Assignment
+        self.cost_matrix = np.array()
 
         # Initialize member variables
         self.graph = gtsam.NonlinearFactorGraph()
@@ -77,9 +111,19 @@ class GraphTracker():
 
     def delete_track_candidates(self):
         self.trk_candidates = [track for track in self.trk_candidates if ((rospy.Time.now() - track.timestamp).to_sec() < self.trk_delete_time)]
-        # for track in self.trk_candidates:
-        #     print((rospy.Time.now() - track.timestamp).to_sec())
+   
+    # Data association
+    def cost(self, det, track):
+        # Euclidean distance between positions
+        return np.linalg.norm(det.pos - track.state.mean()[0:3])
 
+    def compute_cost_matrix(self):
+        self.cost_matrix = np.zeros((len(self.detections),len(self.tracks)))
+        for ii,det in enumerate(self.detections):
+            for jj,trk in enumerate(self.tracks):
+                self.cost_matrix[ii,jj] = self.cost(det,trk)
+
+    # Detection callback / main algorithm
     def det_callback(self, det_array_msg):
         
         # Populate detections list from detections message
@@ -88,39 +132,35 @@ class GraphTracker():
             self.pose_stamped = self.tf_buf.transform(PoseStamped(det.header,det.pose), self.frame_id, rospy.Duration(1))
             self.detections.append(Detection(det.header.stamp, self.pose_stamped.pose.position.x, self.pose_stamped.pose.position.y, self.pose_stamped.pose.position.z, det.value, det.label))
 
-        # TODO - propagate existing tracks and candidates     
+        # Propagate existing tracks
+        for trk in self.tracks:
+            trk.predict(det_array_msg.header.stamp)
         
-        # TODO - Compute detection-track correspondences
-        
+        # Compute detection-track correspondences
+        self.compute_cost_matrix()
+        self.solve_cost_matrix()
+
         # TODO - Assign detections to track variables
         # Add factors, variables
-        # 
         
+        # TODO - Update tracks
+        # for trk in self.tracks:
+        #     trk.predict(det_array_msg.header.stamp)
+
         # Save unmatched detections as potential track candidates
         while self.detections:
             self.trk_candidates.append(Track(self.detections.pop()))
-
-        # TODO - create tracks from candidates
-
-        # TODO - Populate and solve graph
-
         
         # Delete tracks as needed
         self.delete_tracks()
         self.delete_track_candidates()
 
-        print(self.detections)
-        print(self.trk_candidates)
-        print()
+        # TODO - Convert tracks to track message
+        # self.trk_msg.boxes = self.tracks
 
         # Publish tracks
         self.trk_msg.header = det_array_msg.header
-        self.trk_msg.boxes = self.tracks
         self.trk_pub.publish()
-
-# Helper functions
-def compute_similarity(det, track):
-    
 
 if __name__ == '__main__':
     # Initialize node
